1d38d00cc1a5b83fd4e88f4f931347aacf1319f4
shiminken <kenwshin@gmail.com>
2022-07-23 21:52:22 +0700

    refactor code

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
-  rewrites() {
-    let rules = [
-      {
-        source: "/api/:path*",
-        destination: "https://personquiz.vercel.app/:path*",
-      },
-    ];
-    return rules;
-  },
 };
 
 module.exports = nextConfig;
--- src/pages/api/[...all].ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import httpProxyMiddleware from 'next-http-proxy-middleware'
-
-export default (req: NextApiRequest, res: NextApiResponse) => httpProxyMiddleware(req, res, {
-     // You can use the `http-proxy` option
-     target:'https://personquiz.vercel.app/api/ggsheet',
-     // In addition, you can use the `pathRewrite` option provided by `next-http-proxy`
-
-});
